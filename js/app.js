e657416db054b65ad3ce68117c946ac550da143c
Nikita Belevich <belevish_1998@mail.ru>
2020-11-12 14:25:33 +0300

    Task 1 is done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 'use strict';
 
 
+
+// const regexp1 = /в/g;
+// const elem1 = document.querySelector('.task1__example');
+
+// findMatchInTheNode(regexp1, elem1);
+// function findMatchInTheNode(regexp, node) {
+//     let elemStrValue = node.textContent;
+//     if (regexp.flags.includes('g')) {
+//         let arrayMatches = elemStrValue.match(regexp);
+//         arrayMatches.forEach(match => {
+            
+//         });
+//         console.warn(foundMatch2);
+//     }
+//     let foundMatch = elemStrValue.match(regexp)[0];
+//     let newNodeValue = elemStrValue.replace(regexp, `<span class="found-matches">${foundMatch}</span>`);
+//     node.innerHTML = newNodeValue;
+
+//     console.dir(regexp.flags);
+// }
+
+// Task 1
+const regexp1 = /\d\d[:-]\d\d/g;
+const elem1 = document.querySelector('.task1__example');
+const elem1Val = elem1.textContent;
+console.log("Task 1: ", elem1Val.match(regexp1));
+
